02af6c573f8837afefb08df90b5a27134c358ff2
banhdeptraibodoithe <levevietnam@gmail.com>
2025-05-28 14:01:27 +0700

    fcm

--- findMax.js
+++ findMax.js
@@ -1,13 +1,19 @@
 let numbers = [];
 let range = +prompt("Nhập kích thước của mảng: ");
-let max;
+let max, positionMax;
 for (let i = 0; i < range; i++)
 {
     let num = +prompt("Nhập phần tử thứ " + (i + 1));
     if (i == 0)
+    {
         max = num;
+        positionMax = i;
+    }
     if (num > max)
+    {
         max = num;
+        positionMax = i;
+    }
     numbers.push(num);
 }
-alert("Giá trị lớn nhất là: " + max);
+alert("Giá trị lớn nhất là: " + max + " tại vị trí " + positionMax);
